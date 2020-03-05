1b28db2c8efbd6ab78448f1c56d8240fc01cf49f
Henry Funk <georgehenryfunk@gmail.com>
2020-03-04 21:17:21 -0800

    first attempt at loops upload

--- js/app.js
+++ js/app.js
@@ -23,6 +23,9 @@ function askName(){
 function askPreference(){
     var preference = prompt('Do you wear a belt?');
     var message;
+    while(preference !== 'yes' && preference !== 'no'){
+        preference = prompt('Do you wear a belt? PLEASE ANSWER \"yes\" or \"no\"');
+    }
     if (preference === 'yes') {
     message = 'Great! You\'ve come to the right place.';
     } else if (preference === 'no') {
@@ -31,4 +34,21 @@ function askPreference(){
     message = 'Everyone deserves a great belt!';
     };
     return document.write('<h3>' + message + '<h3>');
+}
+
+function howMany(){
+    var count = prompt('How many belts would you like?');
+    while (count === '' || isNaN(count)){
+        var count = prompt('How many belts would you like? ENTER A NUMBER!');
+    }
+    return count;
+}
+function showOrder(){
+    var message = askPreference();
+    var total = howMany();
+    result = '';
+    for (var i = 0; i < total; i++){
+        result = result + '<p>' + message + '</p>';
+    }
+return document.write(result);
 }
